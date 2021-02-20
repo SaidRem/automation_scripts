1fda1b440ea6dac04ba02f9f160dd7cd5b522819
SaidRem <said.r@yandex.ru>
2021-02-20 18:46:59 +0400

    Added func and comments

--- unzip_zips.py
+++ unzip_zips.py
@@ -2,11 +2,14 @@ from glob import glob
 import zipfile
 import sys
 
-zip_files = glob('*.zip')
 
-if zip_files:
-    for z in zip_files:
-        zipfile.ZipFile(zr 'r').extractall()
-else:
-    print("Zip files not found")
+
+def unzipping():
+    zip_files = glob('*.zip')    # List of all zip archives in current folder
+
+    if zip_files:
+        for z in zip_files:
+            zipfile.ZipFile(zr 'r').extractall()
+    else:
+        print("Zip files not found")
 
