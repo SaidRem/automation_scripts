a5a3c75a5d0627b9409492473da0b482b5d5665d
SaidRem <said.r@yandex.ru>
2021-02-22 18:43:09 +0400

    Added func

--- paste.py
+++ paste.py
@@ -14,14 +14,19 @@ DATA = {'t1': 'data1', # TODO replace 't' keys and 'data' by useful data
         't5': 'data5'}
 
 
-if len(sys.argv) < 2:
-    print('Usage: paste.py [key] => copy data to clipboard')
-    sys.exit()
+def main():
+    if len(sys.argv) < 2:
+        print('Usage: paste.py [key] => copy data to clipboard')
+        sys.exit()
+    
+    key = sys.argv[1]     # first command line arg is a key to access data
+    
+    if key in DATA:
+        pyperclip.copy(DATA[key])
+        print(f'Data for {key} copied to clipboard.')
+    else:
+        print(f'There is no key named {key}')
 
-key = sys.argv[1]     # first command line arg is a key to access data
 
-if key in DATA:
-    pyperclip.copy(DATA[key])
-    print(f'Data for {key} copied to clipboard.')
-else:
-    print(f'There is no key named {key}')
+if __name__ == "__main__":
+    main()
