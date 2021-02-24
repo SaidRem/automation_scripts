5d7c01107ca51e848f6764e919801542af2bb010
SaidRem <said.r@yandex.ru>
2021-02-24 17:36:25 +0400

    Added sys argv functionality with separated sign option

--- clipboard_separator.py
+++ clipboard_separator.py
@@ -13,11 +13,25 @@ def splitter(text):
             prom = input('More? (y/n) => ').strip()
             if prom != 'y':
                 pyperclip.copy('')
-                break
+                return
+        pyperclip.copy('')
+    
+    elif len(sys.argv) == 2:
+        sep = sys.argv[1]
+        result = text.split(sep)
+        while result:
+            pyperclip.copy(result[0])
+            print('A part of the text in the clipboard')
+            result = result[1:]
+            prom = input('More? (y/n) => ').strip()
+            if prom != 'y':
+                pyperclip.copy('')
+                return
         pyperclip.copy('')
 
+
 # TODO
-# sys.argv with separted sign in
+# replace func
 
 
 
