296e94ffd5649eec1a959b412f8d798167787aaf
SaidRem <said.r@yandex.ru>
2021-04-29 22:08:45 +0400

    Added shelve dbase. Changed paste module

--- paste.py
+++ paste.py
@@ -1,31 +1,23 @@
 """
 The script works in command line.
-Takes one command line argument after the script name for key in DATA
-and retrieves by the key the data in DATA dict and places the data to the clipboard.
+
 """ 
 
 import sys
 import pyperclip
-
-DATA = {'t1': 'data1', # replace 't' keys and 'data' by useful data
-        't2': 'data2',
-        't3': 'data3',
-        't4': 'data4',
-        't5': 'data5'}
-
+import shelve
 
 def main():
-    if len(sys.argv) < 2:
-        print('Usage: paste.py [key] => copy data to clipboard')
-        sys.exit()
-    
-    key = sys.argv[1]     # first command line arg is a key to access data
-    
-    if key in DATA:
-        pyperclip.copy(DATA[key])
-        print(f'Data for {key} copied to clipboard.')
-    else:
-        print(f'There is no key named {key}')
+    with shelve.open('clipboard_storage') as storage:
+        if len(sys.argv) == 3 and sys.argv[1].lower() == 'save':
+            storage[sys.argv[2]] = pyperclip.paste()
+        elif len(sys.argv) == 2:
+            if sys.argv[1].lower() == 'list':
+                pyperclip.copy(str(list(storage.keys())))
+            elif sys.argv[1] in storage:
+                pyperclip.copy(storage[sys.arg[1]])
+            else:
+                pyperclip.copy('Not such key')
 
 
 if __name__ == "__main__":
