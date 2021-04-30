00856b9544429232647053b09729f8ca50182cd8
SaidRem <said.r@yandex.ru>
2021-04-30 22:31:39 +0400

    Added comments

--- paste.py
+++ paste.py
@@ -1,6 +1,9 @@
 """
 The script works in command line.
-
+Usage:
+>> paste.py save <keyword>  # => saves clipboard to keyword.
+>> paste.py <keyword>       # => loads from database by the <keyword> to clipboard.
+>> paste allkeys            # => loads all keyword to clipboard.
 """ 
 
 import sys
@@ -12,7 +15,7 @@ def main():
         if len(sys.argv) == 3 and sys.argv[1].lower() == 'save':
             storage[sys.argv[2]] = pyperclip.paste()
         elif len(sys.argv) == 2:
-            if sys.argv[1].lower() == 'list':
+            if sys.argv[1].lower() == 'allkeys':
                 pyperclip.copy(str(list(storage.keys())))
             elif sys.argv[1] in storage:
                 pyperclip.copy(storage[sys.arg[1]])
