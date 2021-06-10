a9a76e261c5f0754a63a8a850ba2be6c3804ee2d
SaidRem <said.r@yandex.ru>
2021-06-10 22:00:12 +0400

    Comments in encrypt_pdf.py

--- PDFs_job/encrypt_pdf.py
+++ PDFs_job/encrypt_pdf.py
@@ -1,10 +1,15 @@
+# Encrypt all pdf files in the folder with the script.
+
 import os
 import PyPDF2
 from pathlib import Path
 
 def encrypt_pdf(your_password):
+    """Encrypting pdf files in current folder and saving in
+    the folder 'encrypted_pdfs'.
+    :param your_password: Password to assign to all pdf files.
+    """
     os.makedirs('encrypted_pdfs', exist_ok=True)
-
     for f in os.listdir():
         if f.endswith('.pdf'):
             with open(f, 'rb') as pdf_file:
